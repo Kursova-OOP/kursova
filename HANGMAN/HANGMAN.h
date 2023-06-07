e1477ab9f0060730c7d48716cec9ea401fb8b4f0
ili03vasileva <116426197+ili03vasileva@users.noreply.github.com>
2023-06-07 11:45:49 +0300

    HANGMAN files

---
+++ HANGMAN/HANGMAN.h
@@ -0,0 +1,27 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+#include <string>
+#include <vector>
+
+class Hangman {
+public:
+    Hangman();
+    void play();
+
+private:
+    std::string getRandomWord();
+    void initializeWordStatus();
+    void displayWordStatus();
+    void updateWordStatus(char guess);
+    bool isGameWon();
+    bool isGameLost();
+    void displayHangman(int incorrectGuesses);
+    void displayIncorrectGuesses();
+
+    std::string secretWord;
+    std::string wordStatus;
+    std::vector<char> incorrectGuesses;
+};
+
+#endif
---
+++ HANGMAN/main.cpp
@@ -0,0 +1,8 @@
+#include "HANGMAN.h"
+
+int main() {
+    Hangman game;
+    game.play();
+
+    return 0;
+}
