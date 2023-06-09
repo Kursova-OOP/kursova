1d1b78389f8b75e4425bac44c07b628621e643cf
Emiliya-Stoyanova <125960945+Emiliya-Stoyanova@users.noreply.github.com>
2023-06-09 21:34:05 +0300

    Update card.cpp

---
+++ WAR/card.cpp
@@ -0,0 +1,48 @@
+#include "card.h"
+#include <iostream>
+
+Card::Card(Suit suit, Rank rank) : suit(suit), rank(rank) {}
+
+int Card::getValue() const {
+    // In this simple version, card value is equal to its rank
+    return static_cast<int>(rank);
+}
+
+void Card::display() const {
+    std::string suitString;
+    std::string rankString;
+
+    switch (suit) {
+        case Suit::CLUBS:
+            suitString = "Clubs";
+            break;
+        case Suit::DIAMONDS:
+            suitString = "Diamonds";
+            break;
+        case Suit::HEARTS:
+            suitString = "Hearts";
+            break;
+        case Suit::SPADES:
+            suitString = "Spades";
+            break;
+    }
+
+    switch (rank) {
+        case Rank::ACE:
+            rankString = "Ace";
+            break;
+        case Rank::JACK:
+            rankString = "Jack";
+            break;
+        case Rank::QUEEN:
+            rankString = "Queen";
+            break;
+        case Rank::KING:
+            rankString = "King";
+            break;
+        default:
+            rankString = std::to_string(static_cast<int>(rank));
+    }
+
+    std::cout << rankString << " of " << suitString;
+}
